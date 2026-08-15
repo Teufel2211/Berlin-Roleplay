0da696415b19b479eb748ed0d6de49bb05490ab9
Teufel2211 <210043657+Teufel2211@users.noreply.github.com>
2026-08-15 18:37:33 +0200

    Remove counting and register team moderation

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,11 +1,12 @@
 const verify = require('./verify');
 const warteraum = require('./warteraum');
 const giveaway = require('./giveaway');
-const counting = require('./counting');
 const application = require('./application');
 const bewerbungVerwalten = require('./bewerbung-verwalten');
 const ticket = require('./ticket');
 const admin = require('./admin');
 const interview = require('./interview');
+const team = require('./team');
+const moderation = require('./moderation');
 
-module.exports = [verify, warteraum, giveaway, counting, application, bewerbungVerwalten, ticket, admin, interview];
+module.exports = [verify, warteraum, giveaway, application, bewerbungVerwalten, ticket, admin, interview, team, moderation];
