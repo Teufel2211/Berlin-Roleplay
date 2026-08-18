3ee32bd74a0cd2d16e555212ef316c129c6c8c2a
Teufel2211 <210043657+Teufel2211@users.noreply.github.com>
2026-08-18 12:17:40 +0200

    Enable giveaway command only

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,2 +1,3 @@
-// Systems temporarily disabled. Keep the command directory intact so features can be reintroduced later.
-module.exports = [];
+const giveaway = require('./giveaway');
+
+module.exports = [giveaway];
