b0806a101c7650a3f19d38b6c1261cc7e70d8bef
Teufel2211 <210043657+Teufel2211@users.noreply.github.com>
2026-08-18 12:16:52 +0200

    Rebuild giveaway slash commands

--- src/commands/giveaway.js
+++ src/commands/giveaway.js
@@ -1,6 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
 const giveawayService = require('../services/giveawayService');
-const settingsService = require('../services/settingsService');
 const embeds = require('../discord/embeds');
 const helpers = require('../discord/helpers');
 
@@ -8,38 +7,41 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName('giveaway')
     .setDescription('Giveaway-Verwaltung')
-    .addSubcommand((s) =>
-      s
-        .setName('starten')
-        .setDescription('Startet ein Giveaway')
-        .addStringOption((o) => o.setName('preis').setDescription('Was gibt es zu gewinnen?').setRequired(true))
-        .addStringOption((o) => o.setName('dauer').setDescription('z.B. 30m, 1h, 2d').setRequired(true))
-        .addIntegerOption((o) => o.setName('gewinner').setDescription('Anzahl Gewinner (Standard aus Einstellungen)'))
+    .addSubcommand((s) => s
+      .setName('create')
+      .setDescription('Erstellt ein Giveaway')
+      .addStringOption((o) => o.setName('name').setDescription('Name des Giveaways').setRequired(true))
+      .addStringOption((o) => o.setName('länge').setDescription('z.B. 30m, 2h, 3d').setRequired(true))
+      .addIntegerOption((o) => o.setName('gewinner').setDescription('Anzahl der Gewinner').setMinValue(1).setRequired(true))
+      .addStringOption((o) => o.setName('extra_entries').setDescription('Rollen mit Bonus-Losen, z.B. @VIP:2,@Team:3'))
+      .addStringOption((o) => o.setName('requirements').setDescription('Voraussetzungen, z.B. @Verifiziert + Account 30 Tage'))
     )
-    .addSubcommand((s) => s.setName('enden').setDescription('Beendet ein Giveaway vorzeitig').addStringOption((o) => o.setName('giveaway-id').setDescription('ID aus /giveaway liste').setRequired(true)))
-    .addSubcommand((s) => s.setName('verlängern').setDescription('Verlängert die Endzeit').addStringOption((o) => o.setName('giveaway-id').setDescription('ID aus /giveaway liste').setRequired(true)).addStringOption((o) => o.setName('dauer').setDescription('z.B. 30m, 1h').setRequired(true)))
-    .addSubcommand((s) => s.setName('neu').setDescription('Zieht Gewinner neu (nur beendete)').addStringOption((o) => o.setName('giveaway-id').setDescription('ID aus /giveaway liste').setRequired(true)))
-    .addSubcommand((s) => s.setName('teilnehmer').setDescription('Zeigt die Teilnehmerliste').addStringOption((o) => o.setName('giveaway-id').setDescription('ID aus /giveaway liste').setRequired(true)))
-    .addSubcommand((s) => s.setName('abbrechen').setDescription('Bricht ein Giveaway ab').addStringOption((o) => o.setName('giveaway-id').setDescription('ID aus /giveaway liste').setRequired(true)))
-    .addSubcommand((s) => s.setName('liste').setDescription('Zeigt laufende Giveaways')),
+    .addSubcommand((s) => s
+      .setName('end')
+      .setDescription('Beendet ein Giveaway')
+      .addStringOption((o) => o.setName('id').setDescription('Giveaway-ID').setRequired(true))
+    )
+    .addSubcommand((s) => s
+      .setName('reroll')
+      .setDescription('Zieht Gewinner neu')
+      .addStringOption((o) => o.setName('id').setDescription('Giveaway-ID').setRequired(true))
+      .addIntegerOption((o) => o.setName('anzahl').setDescription('Wie viele Gewinner neu gezogen werden').setMinValue(1).setRequired(true))
+      .addBooleanOption((o) => o.setName('sieger_behalten').setDescription('Bereits gezogene Sieger behalten'))
+    ),
   async execute(interaction) {
+    if (!interaction.guild) return interaction.reply({ embeds: [embeds.error('Nur auf einem Server', 'Dieser Command funktioniert nur auf einem Discord-Server.', interaction.guild)], ephemeral: true });
+    if (!helpers.isGuildModerator(interaction.member, {})) {
+      return interaction.reply({ embeds: [embeds.error('Keine Berechtigung', 'Du darfst keine Giveaways verwalten.', interaction.guild)], ephemeral: true });
+    }
     const sub = interaction.options.getSubcommand();
-    const settings = await settingsService.getAll(interaction.guild.id);
-    const guild = interaction.guild;
-    const t = await require('../i18n').getT(guild.id);
-    if (!helpers.isGuildModerator(interaction.member, settings)) {
-      return interaction.reply({ embeds: [embeds.error(t('giveaway.noPermission.title'), t('giveaway.noPermission.msg'), guild)], ephemeral: true });
-    }
-
-    if (sub === 'starten') {
-      return giveawayService.createGiveaway(interaction, interaction.options.getString('preis'), interaction.options.getString('dauer'), interaction.options.getInteger('gewinner'));
-    }
-    const id = interaction.options.getString('giveaway-id');
-    if (sub === 'enden') return giveawayService.endNow(interaction, id);
-    if (sub === 'verlängern') return giveawayService.extend(interaction, id, interaction.options.getString('dauer'));
-    if (sub === 'neu') return giveawayService.redraw(interaction, id);
-    if (sub === 'teilnehmer') return giveawayService.participantsList(interaction, id);
-    if (sub === 'abbrechen') return giveawayService.cancel(interaction, id);
-    if (sub === 'liste') return giveawayService.list(interaction);
+    if (sub === 'create') return giveawayService.createGiveaway(interaction, {
+      name: interaction.options.getString('name'),
+      duration: interaction.options.getString('länge'),
+      winners: interaction.options.getInteger('gewinner'),
+      extraEntries: interaction.options.getString('extra_entries') || '',
+      requirements: interaction.options.getString('requirements') || '',
+    });
+    if (sub === 'end') return giveawayService.endNow(interaction, interaction.options.getString('id'));
+    return giveawayService.reroll(interaction, interaction.options.getString('id'), interaction.options.getInteger('anzahl'), interaction.options.getBoolean('sieger_behalten') !== false);
   },
 };
