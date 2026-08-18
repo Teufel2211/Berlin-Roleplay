61245248d2c420a11e8f8aa274d93fd8242c2136
Teufel2211 <210043657+Teufel2211@users.noreply.github.com>
2026-08-18 12:13:36 +0200

    Temporarily remove all bot commands

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,12 +1,2 @@
-const verify = require('./verify');
-const warteraum = require('./warteraum');
-const giveaway = require('./giveaway');
-const application = require('./application');
-const bewerbungVerwalten = require('./bewerbung-verwalten');
-const ticket = require('./ticket');
-const admin = require('./admin');
-const interview = require('./interview');
-const team = require('./team');
-const moderation = require('./moderation');
-
-module.exports = [verify, warteraum, giveaway, application, bewerbungVerwalten, ticket, admin, interview, team, moderation];
+// Systems temporarily disabled. Keep the command directory intact so features can be reintroduced later.
+module.exports = [];
